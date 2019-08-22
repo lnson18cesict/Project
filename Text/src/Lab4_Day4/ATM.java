ff0bfc352c37921a31826ccd7eaac282717e6516
lengocson99 <lnson.18ce@sict.udn.vn>
2019-08-22 18:12:05 +0700

    no message

---
+++ Text/src/Lab4_Day4/ATM.java
@@ -0,0 +1,9 @@
+package Lab4_Day4;
+
+public interface ATM {
+	boolean withdraw(int id, double soTien);
+	boolean diposit(int id, double soTien);
+	double queryBalance(int id);
+	boolean login(String user, String password);
+	boolean logout(String user);
+}
---
+++ Text/src/Lab4_Day4/ATMImpl.java
@@ -0,0 +1,55 @@
+package Lab4_Day4;
+
+public class ATMImpl implements ATM {
+	private int idATM;
+	private int accountid;
+	public ATMImpl(int idATM, int accountid) {
+		super();
+		this.idATM = idATM;
+		this.accountid = accountid;
+	}
+	public int getIdATM() {
+		return idATM;
+	}
+	public void setIdATM(int idATM) {
+		this.idATM = idATM;
+	}
+	public int getAccountid() {
+		return accountid;
+	}
+	public void setAccountid(int accountid) {
+		this.accountid = accountid;
+	}
+	@Override
+	public String toString() {
+		return "ATMImpl [idATM=" + idATM + ", accountid=" + accountid + "]";
+	}
+	@Override
+	public boolean withdraw(int id, double soTien) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean diposit(int id, double soTien) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public double queryBalance(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public boolean login(String user, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean logout(String user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+	
+}
